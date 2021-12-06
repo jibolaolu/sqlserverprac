0fd5dcf37db4089743e27c19dee588ad76b27382
jibolaolu <seun_2006@yahoo.com>
2021-12-06 03:36:10 +0000

    Change in Schema

--- Product.sql
+++ Product.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(30) NULL, 
     [Price] INT NULL, 
-    [Color] NCHAR(10) NULL
+    [Color] NCHAR(10) NULL, 
+    [Department] NCHAR(20) NULL
 )
